7a4e77d4a54e07a7add85c4104f0b913471b16b9
Christopher J Chizinski <cchizinski2@unl.edu>
2023-10-22 12:19:02 -0500

    attempting docker image

---
+++ convert-Rmd-script.R
@@ -0,0 +1,18 @@
+## Script file to convert file to R script from a markdown file
+
+hmw_filez <- list.files(here::here("assignment"),
+                        pattern = '.*\\.(R|r|rmd|Rmd|Qmd|qmd)$')
+
+full_path <- here::here("assignment", hmw_filez) # full path to file
+
+if(!file.exists("feedback")){ # create a feedback folder if it doesnt exist
+  dir.create("feedback")
+  }
+
+fextension <- tools::file_ext(full_path) # check file extension
+
+if(stringr::str_detect(fextension, 'md')){
+  knitr::purl(full_path, output = here::here("feedback",paste0(tools::file_path_sans_ext(hmw_filez), '.R'))) # convert to R script
+  
+  full_path <- here::here("feedback",paste0(tools::file_path_sans_ext(hmw_filez), '.R')) # update full path
+  }
